35877b2d6434cf8a0a90f8818afc47a4e38c6bd9
onni82 <onni.bucht@me.com>
2025-09-17 13:08:24 +0200

    defined types in TS

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export type Customer = {
+  id?: number
+  name: string
+  phoneNumber: string
+}
+
+export type Table = {
+  id: number
+  tableNumber: number
+  capacity: number
+}
+
+export type Booking = {
+  id: number
+  tableId: number
+  customerId: number
+  start: string // ISO string
+  guests: number
+}
+
+export type BookingResult = Booking
