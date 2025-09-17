a7d2443961ecc619f7ac9c3e18fb799249d376c9
onni82 <onni.bucht@me.com>
2025-09-17 14:26:47 +0200

    updated main to tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BookingProvider } from './context/BookingContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BookingProvider>
     <App />
